77a4baff29429814de194a11a030ca9bfb47a1e1
effie882 <effiemorara@gmail.com>
2025-07-26 19:02:43 +0300

    complete lab

--- src/components/App.js
+++ src/components/App.js
@@ -2,17 +2,14 @@ import React from "react";
 import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
-
-// pass this data down as props to the child component(s) that need it!
-import user from "../data/user";
-console.log(user);
+import { name, city, bio, github, linkedin } from "../data/user";
 
 function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home name={name} city={city} />
+      <About bio={bio} github={github} linkedin={linkedin} />
     </div>
   );
 }
--- src/data/user.js
+++ src/data/user.js
@@ -1,12 +1,20 @@
 const user = {
-  name: "Liza",
-  city: "New York",
-  bio: "I made this!",
+  name: "Effie",
+  city: "Nairobi",
+  bio: "I am a React developer!",
   color: "firebrick",
   links: {
-    github: "https://github.com/liza",
-    linkedin: "https://www.linkedin.com/in/liza/",
-  },
+    github: "https://github.com/effie",
+    linkedin: "https://linkedin.com/in/effie"
+  }
 };
 
 export default user;
+
+export const name = user.name;
+export const city = user.city;
+export const bio = user.bio;
+export const color = user.color;
+export const github = user.links.github;
+export const linkedin = user.links.linkedin;
+
